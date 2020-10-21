78a8b042530d73ab93a6a0e27b9cad09391dd1ab
quedacoder <quedacoder@gmail.com>
2020-10-21 12:22:20 -0500

    initial commit

---
+++ src/main/java/com/quedacoder/taskstracker/TasksTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.quedacoder.taskstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quedacoder/taskstracker/TasksTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quedacoder.taskstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
