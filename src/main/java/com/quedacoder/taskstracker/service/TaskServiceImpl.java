ac547246baabb1c295854c499e98fb5a6018c4bb
quedacoder <quedacoder@gmail.com>
2020-10-21 21:41:23 -0500

    Fix bug with autowiring

--- src/main/java/com/quedacoder/taskstracker/service/TaskServiceImpl.java
+++ src/main/java/com/quedacoder/taskstracker/service/TaskServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.quedacoder.taskstracker.model.Task;
 import com.quedacoder.taskstracker.repository.TaskRepository;
 
+@Service
 public class TaskServiceImpl implements TaskService {
 	
 	
